932bf604261354003028ce35892ef9ab03f72020
Elwyn Davies <davieseb@scss.tcd.ie>
2012-12-13 16:17:45 +0000

    Fix accidental removal of code.

--- python/nilib/wsgishim.py
+++ python/nilib/wsgishim.py
@@ -1526,5 +1526,14 @@ class wsgiHTTPRequestShim:
     #--------------------------------------------------------------------------#
     def log_date_time_string(self):
         """
+        @brief Return the current time formatted for logging.
+        @return string with curent time formatted suitable for logging.
+        """
+        now = time.time()
+        year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
+        s = "%02d/%3s/%04d %02d:%02d:%02d" % (
+                day, self.MONTHNAME[month], year, hh, mm, ss)
+        return s
+
 #==============================================================================#
 
